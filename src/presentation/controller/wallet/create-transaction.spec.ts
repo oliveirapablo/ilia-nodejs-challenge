72cceb2914b6befc57f222cadad44120cf2a9785
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-01-23 20:38:35 -0300

    test: add AddTransaction to controller an if it throws returns 500 from provided and it was returns success

--- src/presentation/controller/wallet/create-transaction.spec.ts
+++ src/presentation/controller/wallet/create-transaction.spec.ts
@@ -1,14 +1,33 @@
 import { CreateTransactionController } from './create-transaction'
 import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
+import { AddTransaction, AddTransactionModel, TransactionModel, TypesTransaction } from './wallet-protocols'
+
+const makeAddTransaction = (): AddTransaction => {
+  class AddATransactionStub implements AddTransaction {
+    async add (transaction: AddTransactionModel): Promise<TransactionModel> {
+      const fakeTransaction = {
+        id: 'valid_id',
+        user_id: 'valid_user_id',
+        amount: 30,
+        type: TypesTransaction.CREDIT
+      }
+      return await new Promise(resolve => resolve(fakeTransaction))
+    }
+  }
+  return new AddATransactionStub()
+}
 
 interface SutTypes {
   sut: CreateTransactionController
+  addTransactionStub: AddTransaction
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new CreateTransactionController()
+  const addTransactionStub = makeAddTransaction()
+  const sut = new CreateTransactionController(addTransactionStub)
   return {
-    sut
+    sut,
+    addTransactionStub
   }
 }
 
@@ -68,5 +87,41 @@ describe('CreateTransactionController', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('type'))
+  })
+
+  test('Should call AddTransaction with correct values', async () => {
+    const { sut, addTransactionStub } = makeSut()
+    const addSpy = jest.spyOn(addTransactionStub, 'add')
+
+    const httpRequest = {
+      body: {
+        user_id: 'any_user_id',
+        type: 'CREDIT',
+        amount: 'any_amount'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith({
+      user_id: 'any_user_id',
+      type: 'CREDIT',
+      amount: 'any_amount'
+    })
+  })
+
+  test('Should return 500 if AddTransaction throws', async () => {
+    const { sut, addTransactionStub } = makeSut()
+    jest.spyOn(addTransactionStub, 'add').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => reject(new Error()))
+    })
+    const httpRequest = {
+      body: {
+        user_id: 'any_user_id',
+        type: 'CREDIT',
+        amount: 'any_amount'
+      }
+    }
+    const httResponse = await sut.handle(httpRequest)
+    expect(httResponse.statusCode).toBe(500)
+    expect(httResponse.body).toEqual(new ServerError())
   })
 })
