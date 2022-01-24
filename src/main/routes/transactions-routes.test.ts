78a82b3cdcfeeb2a35ea9bf88f6627bf6438efc1
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-01-24 09:09:39 -0300

    refactor: connect to mongodb before tests

--- src/main/routes/transactions-routes.test.ts
+++ src/main/routes/transactions-routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Transactions Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('transactions')
+    await accountCollection.deleteMany({})
+  })
   test('Shoul return an transaction on success', async () => {
     await request(app)
       .post('/transactions')
