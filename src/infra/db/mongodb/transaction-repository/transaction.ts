3b3b9a7c225713ecee0e9133f4cc14ae775b28b5
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-01-24 01:31:42 -0300

    feat: ensure TransactionMongoRepository create an transaction on success

---
+++ src/infra/db/mongodb/transaction-repository/transaction.ts
@@ -0,0 +1,13 @@
+import { AddTransactionRepository } from '../../../../data/protocols/add-transaction-repository'
+import { AddTransactionModel, TransactionModel } from '../../../../presentation/protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class TransactionMongoRepository implements AddTransactionRepository {
+  async add (transactionData: AddTransactionModel): Promise<TransactionModel> {
+    const transactionCollection = MongoHelper.getCollection('transactions')
+    const result = await transactionCollection.insertOne(transactionData)
+    const idInserted = result.insertedId.toString()
+    console.log(idInserted)
+    return { id: idInserted, ...transactionData }
+  }
+}
