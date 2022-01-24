064a6816e23acfac50dfe4a7b23ee730a7601e52
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-01-24 01:32:07 -0300

    test: ensure TransactionMongoRepository create an transaction on success

---
+++ src/infra/db/mongodb/transaction-repository/transaction.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { TransactionMongoRepository } from './transaction'
+enum TypesTransaction {
+  CREDIT = 'CREDIT',
+  DEBIT = 'DEBIT'
+}
+
+describe('Transaction Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('Shoul return an transaction on success', async () => {
+    const sut = new TransactionMongoRepository()
+    const transaction = await sut.add({
+      user_id: 'valid_user_id',
+      amount: 30,
+      type: TypesTransaction.CREDIT
+    })
+
+    expect(transaction).toBeTruthy()
+    expect(transaction.id).toBeTruthy()
+    expect(transaction.user_id).toBe('valid_user_id')
+    expect(transaction.type).toBe('CREDIT')
+    expect(transaction.amount).toBe(30)
+  })
+})
